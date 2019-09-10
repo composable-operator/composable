0e31f570bc606e69b7043a2449b92a0e01919b55
Alexey Roytman <roytman@il.ibm.com>
2019-09-10 16:04:54 +0300

    update test messages (#23)
    * update test messages
    
    * add configurable SyncPeriod

--- pkg/controller/composable/composable_controller_test.go
+++ pkg/controller/composable/composable_controller_test.go
@@ -75,7 +75,8 @@ var _ = BeforeSuite(func() {
 		log.Fatal(err)
 	}
 
-	mgr, err := manager.New(cfg, manager.Options{})
+	syncPeriod := 30 * time.Second // set a sync period
+	mgr, err := manager.New(cfg, manager.Options{SyncPeriod: &syncPeriod})
 	Expect(err).NotTo(HaveOccurred())
 
 	c = mgr.GetClient()
@@ -228,11 +229,11 @@ var _ = Describe("test Composable operations", func() {
 		//unstrObj.SetGroupVersionKind(gvkOut)
 		// First, the output object is created with defult values, after that we deploy the inputObject and will check
 		// that all Output object filed are updated.
-		By("check input object") // the object should not exsist
+		By("check that input object doesn't exist") // the object should not exist
 		unstrObj.SetGroupVersionKind(gvkIn)
 		Ω(test.GetUnstructuredObject(scontext, objNamespacednameIn, &unstrObj)()).Should(HaveOccurred())
 
-		By("check ouput object") // the object should not exist, or we delete it
+		By("check taht output object doesn't exist. If it does => remove it ") // the object should not exist, or we delete it
 		unstrObj.SetGroupVersionKind(gvkOut)
 		err2 := test.GetUnstructuredObject(scontext, objNamespacednameOut, &unstrObj)()
 		if err2 == nil {
@@ -240,32 +241,32 @@ var _ = Describe("test Composable operations", func() {
 			Eventually(test.GetObject(scontext, &unstrObj)).Should(BeNil())
 		}
 
-		By("Deploy Composable object")
+		By("deploy Composable object")
 		comp := test.LoadCompasable(dataDir + "compCopy.yaml")
 		test.PostInNs(scontext, &comp, true, 0)
 		Eventually(test.GetObject(scontext, &comp)).ShouldNot(BeNil())
 
-		By("Get Output object")
+		By("get Output object")
 		unstrObj.SetGroupVersionKind(gvkOut)
 		Eventually(test.GetUnstructuredObject(scontext, objNamespacednameOut, &unstrObj)).Should(Succeed())
 		testSpec, ok := unstrObj.Object[spec].(map[string]interface{})
 		Ω(ok).Should(BeTrue())
 
 		// Check some of default the values
-		By("first default intValue")
+		By("check default intValue")
 		Ω(testSpec["intValue"]).Should(BeEquivalentTo(10))
 
-		By("default stringFromBase64")
+		By("check default stringFromBase64")
 		Ω(testSpec["stringFromBase64"]).Should(Equal("default"))
 
-		By("Deploy input Object")
+		By("deploy input Object")
 		obj := test.LoadObject(dataDir+"inputDataObject.yaml", &unstructured.Unstructured{})
 		test.CreateObject(scontext, obj, true, 0)
 
 		unstrObj.SetGroupVersionKind(gvkIn)
 		Eventually(test.GetUnstructuredObject(scontext, objNamespacednameIn, &unstrObj)).Should(Succeed())
 
-		By("get updated inValue")
+		By("check updated inValue")
 		unstrObj = unstructured.Unstructured{}
 		unstrObj.SetGroupVersionKind(gvkOut)
 		Eventually( func() (int64, error) {
@@ -278,28 +279,28 @@ var _ = Describe("test Composable operations", func() {
 		}).Should(Equal(int64(12)))
 
 		// Check other values
-		By("updated floatValue")
+		By("check updated floatValue")
 		Ω(testSpec["floatValue"].(float64)).Should(BeEquivalentTo(23.5))
 
-		By("updated boolValue")
+		By("check updated boolValue")
 		Ω(testSpec["boolValue"]).Should(BeTrue())
 
-		By("updated stringValue")
+		By("check updated stringValue")
 		Ω(testSpec["stringValue"]).Should(Equal("Hello world"))
 
-		By("updated stringFromBase64")
+		By("check updated stringFromBase64")
 		Ω(testSpec["stringFromBase64"]).Should(Equal("9376"))
 
-		By("updated arrayStrings")
+		By("check updated arrayStrings")
 		Ω(testSpec["arrayStrings"]).Should(Equal(strArray))
 
-		By("updated arrayIntegers")
+		By("check updated arrayIntegers")
 		Ω(testSpec["arrayIntegers"]).Should(Equal([]interface{}{int64(1),int64(2),int64(3),int64(4)}))
 
-		By("updated objectValue")
+		By("check updated objectValue")
 		Ω(testSpec["objectValue"]).Should(Equal(map[string]interface {}{"family": "FamilyName", "first": "FirstName", "age": int64(27)}))
 
-		By("updated stringJson2Value")
+		By("check updated stringJson2Value")
 		val, _ := Array2CSStringTransformer(strArray)
 		Ω(testSpec["stringJson2Value"]).Should(BeEquivalentTo(val))
 	})
